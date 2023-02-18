8c4b3848871bae2f27e6cbe15bc4987e5f567203
Gourab Karmakar <gourabk7458@gmail.com>
2023-02-18 20:59:54 +0530

    added SeleniumDemoTest file

---
+++ src/main/java/selenium/SeliniumDemoTest.java
@@ -0,0 +1,33 @@
+package selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeliniumDemoTest {
+	public static void main(String args[])
+    {
+        System.setProperty(
+            "webdriver.chrome.driver",
+            "C:\\Users\\goura\\eclipse-workspace2\\MavenSele\\Driver\\chromedriver.exe");
+  
+        ChromeDriver driver = new ChromeDriver();
+  
+        // Maximize the browser
+        // using maximize() method
+        driver.manage().window().maximize();
+  
+        // Launching website
+        driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("pics");
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).click();
+        try {
+			Thread.sleep(5000);
+			driver.close();
+			driver.quit();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
